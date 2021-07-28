6077315747427dc8a779b39f3c9c7b132493da4d
seohyeonKK <sts05158@gmail.com>
2021-07-28 17:05:43 +0900

    feat: tag에 해당하는 버튼 생성

---
+++ src/components/ClassificationTags.js
@@ -0,0 +1,73 @@
+import { Button } from 'reactstrap'
+
+const Tags = [
+  {
+    id: 1,
+    name: '알고리즘',
+  },
+  {
+    id: 2,
+    name: '컴퓨터구조',
+  },
+  {
+    id: 3,
+    name: '데이터베이스',
+  },
+  {
+    id: 4,
+    name: '네트워크',
+  },
+  {
+    id: 5,
+    name: '운영체제',
+  },
+  {
+    id: 6,
+    name: '소프트웨어공학',
+  },
+  {
+    id: 7,
+    name: 'Java',
+  },
+  {
+    id: 8,
+    name: 'JavaScript',
+  },
+  {
+    id: 9,
+    name: 'Python',
+  },
+  {
+    id: 10,
+    name: '백엔드',
+  },
+  {
+    id: 11,
+    name: '프론트엔드',
+  },
+  {
+    id: 12,
+    name: '머신러닝',
+  },
+  {
+    id: 13,
+    name: 'iOS',
+  },
+  {
+    id: 14,
+    name: '안드로이드',
+  },
+]
+const ClassificationTags = () => {
+  const TagButtons = Tags.map((tagItem) => {
+    return (
+      <div>
+        <Button color="secondary">{tagItem.name}</Button>
+      </div>
+    )
+  })
+
+  return <div>{TagButtons}</div>
+}
+
+export default ClassificationTags
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import MainCarousel from '../components/MainCarousel'
 import Navigation from '../components/Navigation'
+import ClassificationTags from '../components/ClassificationTags'
 
 function Main() {
   return (
     <div className="Main">
       <Navigation />
       <MainCarousel />
+      <ClassificationTags />
     </div>
   )
 }
