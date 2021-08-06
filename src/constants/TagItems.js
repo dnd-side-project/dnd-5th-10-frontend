33a80e0a9e97e9803147546b3c537c0aec05e805
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-08-07 00:28:15 +0900

    chore: tag 데이터 따로 분리

---
+++ src/constants/TagItems.js
@@ -0,0 +1,60 @@
+const tagItems = [
+  {
+    id: 1,
+    name: '알고리즘',
+  },
+  {
+    id: 2,
+    name: '컴퓨터구조',
+  },
+  {
+    id: 3,
+    name: '데이터베이스',
+  },
+  {
+    id: 4,
+    name: '네트워크',
+  },
+  {
+    id: 5,
+    name: '운영체제',
+  },
+  {
+    id: 6,
+    name: '소프트웨어공학',
+  },
+  {
+    id: 7,
+    name: 'Java',
+  },
+  {
+    id: 8,
+    name: 'JavaScript',
+  },
+  {
+    id: 9,
+    name: 'Python',
+  },
+  {
+    id: 10,
+    name: '백엔드',
+  },
+  {
+    id: 11,
+    name: '프론트엔드',
+  },
+  {
+    id: 12,
+    name: '머신러닝',
+  },
+  {
+    id: 13,
+    name: 'iOS',
+  },
+  {
+    id: 14,
+    name: '안드로이드',
+  },
+]
+
+export default tagItems
