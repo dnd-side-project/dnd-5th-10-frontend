f7c845b98507d7929cf9a02b05453890f0ce06c8
seohyeonKK <sts05158@gmail.com>
2021-08-08 16:09:21 +0900

    feat: 문제 리스트 무한 스크롤 구현

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,48 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import 'css/Navigation.css'
 import axios from 'axios'
 import { JWT_TOKEN } from 'constants/Oauth'
+import { useInView } from 'react-intersection-observer'
 import Question from 'components/Question'
 
 const QuestionList = () => {
   const [allQuestions, setAllQuestions] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(0)
+  const [ref, inView] = useInView()
+
+  const getQuestions = useCallback(async () => {
+    let questions = allQuestions
+    setLoading(true)
+    await axios
+      .get(`/api/v1/question/all?page=${page}&size=5`)
+      .then((res) => {
+        res.data.forEach((item) => {
+          questions.push(item)
+        })
+        setAllQuestions(questions)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        setLoading(false)
+      })
+  })
 
   useEffect(() => {
-    if (JWT_TOKEN) {
-      axios
-        .get(`/api/v1/question/all/`)
-        .then((res) => {
-          let questions = []
-          res.data.forEach((item) => {
-            questions.push(item)
-          })
-          setAllQuestions(questions)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  }, [])
+    getQuestions()
+  }, [page])
 
-  const showQuestions = allQuestions.map((ques, index) => {
-    let quesContent = ques.content
-    return (
-      <Question
-        key={ques.id}
-        id={ques.id}
-        number={index + 1}
-        content={quesContent}
-        username={ques.username}
-        tagList={ques.tagList}
-        bookmarkCount={ques.bookmarkCount}
-      />
-    )
-  })
+  useEffect(() => {
+    if (inView && !loading) {
+      setPage((p) => p + 1)
+    }
+  }, [inView, loading])
 
   return (
     <div>
-      {showQuestions}
-      {console.log(allQuestions)}
+      {allQuestions.map((ques, index) => (
+        <div key={index}>
+          {allQuestions.length - 1 === index ? (
+            <div ref={ref}>
+              <Question
+                key={ques.id}
+                id={ques.id}
+                number={index + 1}
+                content={ques.content}
+                username={ques.username}
+                tagList={ques.tagList}
+                bookmarkCount={ques.bookmarkCount}
+              />
+            </div>
+          ) : (
+            <div>
+              <Question
+                key={ques.id}
+                id={ques.id}
+                number={index + 1}
+                content={ques.content}
+                username={ques.username}
+                tagList={ques.tagList}
+                bookmarkCount={ques.bookmarkCount}
+              />
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   )
 }
