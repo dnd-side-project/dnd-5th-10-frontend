078621becb8547a9e4034aee105d5959734a9d39
seohyeonKK <sts05158@gmail.com>
2021-08-17 18:31:49 +0900

    style: 메인페이지 이미지 변경

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -8,8 +8,15 @@ const MainCarousel = () => {
   return (
     <div id="carousel-slide">
       <div id="background">
-        <img className="space1" src="/img/main_img.png" alt="" />
-
+        <img className="space1" src="/img/main_img2.png" alt="" />
+        <div className="iterview-info">
+          <h1>IT'erview를 소개합니다.</h1>
+          <span>
+            IT'erview는 개발자들의 면접을 효율적으로 도와주는 서비스입니다.
+            <br />
+            체계적인 면접학습을 경험해보세요.
+          </span>
+        </div>
         <Link to="/SetQuizOptions">
           <button className="main-quiz-btn">퀴즈풀기</button>
         </Link>
