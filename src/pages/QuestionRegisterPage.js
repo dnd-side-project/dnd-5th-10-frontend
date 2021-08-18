bf558cb9d56a515f2f773ff869b4dbafaf6c9a8c
seohyeonKK <sts05158@gmail.com>
2021-08-19 05:34:05 +0900

    feat: 문제 등록 이후 완료 안내

--- src/pages/QuestionRegisterPage.js
+++ src/pages/QuestionRegisterPage.js
@@ -7,14 +7,22 @@ import Footer from 'components/Footer'
 axios.defaults.headers.common['Authorization'] = `Bearer ${JWT_TOKEN}`
 
 const QuestionRegisterPage = () => {
-  const questionRegisterImg = '/img/questionRegister.jpg'
+  const questionRegisterImg = '/img/page_info.png'
   return (
     <>
       <Navigation />
+      <img id="deem" src="/img/navigation-deem.png" />
       <div className="question-register-img">
-        <img src={questionRegisterImg} alt="question-register-img" />
-        <h1>문제 등록 페이지</h1>
-        <h3>문제를 등록해주셈</h3>
+        <div className="question-search-info-background">
+          <img src={questionRegisterImg} alt="question-register-img" />
+        </div>
+
+        <h1>면접문제 등록</h1>
+        <h3>
+          답변을 확인하고 싶은 문제가 있으신가요?
+          <br />
+          직접 문제를 등록하고 내 답변을 비교해보세요!
+        </h3>
       </div>
       <div className="body">
         <QuestionRegister />
