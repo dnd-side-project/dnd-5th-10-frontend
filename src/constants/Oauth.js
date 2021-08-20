00d7f01cb31ad5c980c8d3223a2d49b201b5ca3f
seohyeonKK <sts05158@gmail.com>
2021-08-20 14:31:19 +0900

    build: 환경 변수 재설정

--- src/constants/Oauth.js
+++ src/constants/Oauth.js
@@ -2,7 +2,7 @@ import { getCookie } from 'components/Cookies.js'
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000'
+export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_OAUTH2_REDIRECT_URI
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorization/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI
